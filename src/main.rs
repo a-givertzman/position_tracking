825e5aa90195c76ef74209b4fcfd0c64b9e45daa
Anton Lobanov <a.givertzman@icloud.com>
2025-09-08 18:17:44 +0300

    CameraService

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::fs::OpenOptions;
+
 use frdm_tools::Image;
 use sal_core::{dbg::Dbg, error::Error};
 use sal_sync::{services::{conf::ConfTree, Service}, thread_pool::ThreadPool};
 
-use crate::modules::{CameraServiceConf, CameraService};
+use crate::modules::{CameraService, CameraServiceConf};
+
 
 mod modules;
 ///
@@ -10,7 +13,10 @@ mod modules;
 fn main() -> Result<(), Error>{
     let dbg = Dbg::own("position-tracking");
     let thread_pool = ThreadPool::new(&dbg, Some(8));
-    let conf = ConfTree::empty();
+    let conf = "config.yaml";
+    let file = OpenOptions::new().read(true).open(conf).map_err(|err| Error::new(&dbg, "main").pass(err.to_string()))?;
+    let conf = serde_yaml::from_reader(file).map_err(|err| Error::new(&dbg, "main").pass(err.to_string()))?;
+    let conf = ConfTree::new_root(conf);
     let conf = CameraServiceConf::new(&dbg, conf);
     let template = Image::load(&conf.template_match.template)?;
     let camera_service = CameraService::new(&dbg, conf, template, thread_pool.scheduler());
